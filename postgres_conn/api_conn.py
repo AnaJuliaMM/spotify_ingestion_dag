c1401339ffa01ad337bdc4c43a58570344dd392f
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-02-22 20:35:14 -0300

    chore: create file to try api request

---
+++ postgres_conn/api_conn.py
@@ -0,0 +1,50 @@
+import psycopg2
+import json
+
+try:
+    # Conectar com o banco de dados
+    conn = psycopg2.connect(
+                host="localhost",
+                user="postgres",
+                password="12345678",
+                database="playlist"
+            )
+    cursor = conn.cursor()
+
+   # Abre o arquivo 'dados.json' para leitura
+    with open('./output.json', 'r', encoding='utf-8') as arquivo:
+    # Carrega os dados JSON do arquivo em um dicionário Python
+        dados = json.load(arquivo)
+
+    # Construção e execução da inserção de dados
+    for item in dados["tracks"]["items"]:
+        try:
+            # Acessar a chave 'track' do objeto item. Chave que armazena as informações das músicas 
+            track = item['track']
+
+            # Extrair os nomes dos artistas e concatenar usando a palavra 'e'
+            nomes_artistas = [artista['name'] for artista in track['album']['artists']]
+            artistas_str = ' e '.join(nomes_artistas)
+            
+            # Tratar strings que contenham apóstrofos
+            track_name = track['name'].replace("'", "''")
+            album_name = track['album']['name'].replace("'", "''")
+            
+            cursor.execute(
+                f"INSERT INTO Musica (nome, duracao_ms, artistas, nome_album, data_lancamento, total_musicas_album) "
+                f"VALUES ('{track_name}', {track['duration_ms']}, '{artistas_str}', '{album_name}', TO_DATE('{track['album']['release_date']}', 'YYYY-MM-DD'), {track['album']['total_tracks']})"
+            )
+
+        except psycopg2.Error as e:
+            print("Erro ao inserir dados no PostgreSQL:", e)
+
+    # Confirmação da operação de inserção de dados e encerramento da conexão com o banco de dados
+    conn.commit()
+
+except Exception as e:
+    print(e)
+finally:
+    cursor.close()
+    conn.close()
+    print('PostgreSQL connection is closed')
+
