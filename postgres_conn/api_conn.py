7498363c8cdd1e90f4ac0602afee81ab12bcbb68
camilayatabe <camila.yatabe@gmail.com>
2024-02-23 14:15:27 -0300

    feat (api_conn): tranforming connection to database and data insertion into a funcion

--- postgres_conn/api_conn.py
+++ postgres_conn/api_conn.py
@@ -9,8 +9,8 @@ def request_token():
     # Dados para a solicitação do token
     token_data = {
         'grant_type': 'client_credentials',
-        'client_id': '',
-        'client_secret': ''
+        'client_id': 'a1e3602181c5430b821cd0adfb764139',
+        'client_secret': '405594b52fb147f999afe2d89e680b35'
     }
 
     # Fazer a solicitação do token
@@ -53,53 +53,51 @@ def request_playlist():
         # Se ocorrer um erro ao fazer a solicitação, imprima o erro
         print("Erro ao fazer solicitação para a API do Spotify:", e)
 
-
-try:
-    # Conectar com o banco de dados
-    conn = psycopg2.connect(
-                host="localhost",
-                user="postgres",
-                password="12345678",
-                database="Spotify_Database"
-            )
-    cursor = conn.cursor()
-
-
-    dados = request_playlist()
+def insert_database():
+    try:
+        # Conectar com o banco de dados
+        conn = psycopg2.connect(
+                    host="localhost",
+                    user="postgres",
+                    password="12345678",
+                    database="Spotify_Database"
+                )
+        cursor = conn.cursor()
 
 
-    # Construção e execução da inserção de dados
-    for item in dados["tracks"]["items"]:
-        try:
-            # Acessar a chave 'track' do objeto item. Chave que armazena as informações das músicas 
-            track = item['track']
-
-            # Extrair os nomes dos artistas e concatenar usando a palavra 'e'
-            nomes_artistas = [artista['name'] for artista in track['album']['artists']]
-            artistas_str = ' e '.join(nomes_artistas)
-            
-            # Tratar strings que contenham apóstrofos
-            track_name = track['name'].replace("'", "''")
-            album_name = track['album']['name'].replace("'", "''")
-            
-            cursor.execute(
-                f"INSERT INTO Musica (nome, duracao_ms, artistas, nome_album, data_lancamento, total_musicas_album) "
-                f"VALUES ('{track_name}', {track['duration_ms']}, '{artistas_str}', '{album_name}', TO_DATE('{track['album']['release_date']}', 'YYYY-MM-DD'), {track['album']['total_tracks']})"
-            )
-
-        except psycopg2.Error as e:
-            print("Erro ao inserir dados no PostgreSQL:", e)
-
-    # Confirmação da operação de inserção de dados e encerramento da conexão com o banco de dados
-    conn.commit()
-
-except Exception as e:
-    print(e)
-finally:
-    cursor.close()
-    conn.close()
-    print('PostgreSQL connection is closed')
+        dados = request_playlist()
 
 
+        # Construção e execução da inserção de dados
+        for item in dados["tracks"]["items"]:
+            try:
+                # Acessar a chave 'track' do objeto item. Chave que armazena as informações das músicas 
+                track = item['track']
 
+                # Extrair os nomes dos artistas e concatenar usando a palavra 'e'
+                nomes_artistas = [artista['name'] for artista in track['album']['artists']]
+                artistas_str = ' e '.join(nomes_artistas)
+                
+                # Tratar strings que contenham apóstrofos
+                track_name = track['name'].replace("'", "''")
+                album_name = track['album']['name'].replace("'", "''")
+                
+                cursor.execute(
+                    f"INSERT INTO Musica (nome, duracao_ms, artistas, nome_album, data_lancamento, total_musicas_album) "
+                    f"VALUES ('{track_name}', {track['duration_ms']}, '{artistas_str}', '{album_name}', TO_DATE('{track['album']['release_date']}', 'YYYY-MM-DD'), {track['album']['total_tracks']})"
+                )
 
+            except psycopg2.Error as e:
+                print("Erro ao inserir dados no PostgreSQL:", e)
+
+        # Confirmação da operação de inserção de dados e encerramento da conexão com o banco de dados
+        conn.commit()
+
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.close()
+        print('PostgreSQL connection is closed')
+
+insert_database();
