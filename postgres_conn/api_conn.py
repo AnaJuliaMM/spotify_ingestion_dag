cd27fd2107941fc630f8d3cea41b4939077442b2
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-02-23 14:06:56 -0300

    chore: add playlist request into a function

--- postgres_conn/api_conn.py
+++ postgres_conn/api_conn.py
@@ -27,80 +27,79 @@ def request_token():
         print(f"Erro status : {token.status_code}") 
         return token.status_code
 
+def request_playlist():
+    try:
+
+        url = "https://api.spotify.com/v1/playlists/1BZo9URfhmlnt67zRYgM79?si=580665a897e54c64/tracks"
+
+        token = request_token()
+        headers = {
+            "Authorization": f"Bearer {token}"
+        }
+
+        # Enviar solicitação GET para a API do Spotify com o token de acesso no cabeçalho de autorização
+        response = requests.get(url, headers=headers)
+
+        # Verificar se a solicitação foi bem-sucedida (código de status 200)
+        if response.status_code == 200:
+            # Converter a resposta JSON em um dicionário Python
+            return response.json()
+        else:
+            # Se a solicitação não for bem-sucedida, imprima o código de status
+            print("Erro ao acessar a playlist. Código de status:", response.status_code)
+            return response.status_code
+
+    except requests.RequestException as e:
+        # Se ocorrer um erro ao fazer a solicitação, imprima o erro
+        print("Erro ao fazer solicitação para a API do Spotify:", e)
+
+
+
 try:
-
-    url = "https://api.spotify.com/v1/playlists/1BZo9URfhmlnt67zRYgM79?si=580665a897e54c64/tracks"
-
-    token = request_token()
-    headers = {
-        "Authorization": f"Bearer {token}"
-    }
-
-    # Enviar solicitação GET para a API do Spotify com o token de acesso no cabeçalho de autorização
-    response = requests.get(url, headers=headers)
-
-    # Verificar se a solicitação foi bem-sucedida (código de status 200)
-    if response.status_code == 200:
-        # Converter a resposta JSON em um dicionário Python
-        dados_playlist = response.json()
-
-        # Agora você pode usar os dados da playlist conforme necessário
-        print("Nome da playlist:", dados_playlist)
-    else:
-        # Se a solicitação não for bem-sucedida, imprima o código de status
-        print("Erro ao acessar a playlist. Código de status:", response.status_code)
-
-except requests.RequestException as e:
-    # Se ocorrer um erro ao fazer a solicitação, imprima o erro
-    print("Erro ao fazer solicitação para a API do Spotify:", e)
-
-# O restante do código está comentado porque não está sendo usado no momento
-
-# try:
-#     # Conectar com o banco de dados
-#     conn = psycopg2.connect(
-#                 host="localhost",
-#                 user="postgres",
-#                 password="12345678",
-#                 database="playlist"
-#             )
-#     cursor = conn.cursor()
+    # Conectar com o banco de dados
+    conn = psycopg2.connect(
+                host="localhost",
+                user="postgres",
+                password="12345678",
+                database="playlist"
+            )
+    cursor = conn.cursor()
 
 
-#     dados = request_api()
+    dados = request_playlist()
    
 
-#     # Construção e execução da inserção de dados
-#     for item in dados["tracks"]["items"]:
-#         try:
-#             # Acessar a chave 'track' do objeto item. Chave que armazena as informações das músicas 
-#             track = item['track']
+    # Construção e execução da inserção de dados
+    for item in dados["tracks"]["items"]:
+        try:
+            # Acessar a chave 'track' do objeto item. Chave que armazena as informações das músicas 
+            track = item['track']
 
-#             # Extrair os nomes dos artistas e concatenar usando a palavra 'e'
-#             nomes_artistas = [artista['name'] for artista in track['album']['artists']]
-#             artistas_str = ' e '.join(nomes_artistas)
+            # Extrair os nomes dos artistas e concatenar usando a palavra 'e'
+            nomes_artistas = [artista['name'] for artista in track['album']['artists']]
+            artistas_str = ' e '.join(nomes_artistas)
             
-#             # Tratar strings que contenham apóstrofos
-#             track_name = track['name'].replace("'", "''")
-#             album_name = track['album']['name'].replace("'", "''")
+            # Tratar strings que contenham apóstrofos
+            track_name = track['name'].replace("'", "''")
+            album_name = track['album']['name'].replace("'", "''")
             
-#             cursor.execute(
-#                 f"INSERT INTO Musica (nome, duracao_ms, artistas, nome_album, data_lancamento, total_musicas_album) "
-#                 f"VALUES ('{track_name}', {track['duration_ms']}, '{artistas_str}', '{album_name}', TO_DATE('{track['album']['release_date']}', 'YYYY-MM-DD'), {track['album']['total_tracks']})"
-#             )
+            cursor.execute(
+                f"INSERT INTO Musica (nome, duracao_ms, artistas, nome_album, data_lancamento, total_musicas_album) "
+                f"VALUES ('{track_name}', {track['duration_ms']}, '{artistas_str}', '{album_name}', TO_DATE('{track['album']['release_date']}', 'YYYY-MM-DD'), {track['album']['total_tracks']})"
+            )
 
-#         except psycopg2.Error as e:
-#             print("Erro ao inserir dados no PostgreSQL:", e)
+        except psycopg2.Error as e:
+            print("Erro ao inserir dados no PostgreSQL:", e)
 
-#     # Confirmação da operação de inserção de dados e encerramento da conexão com o banco de dados
-#     conn.commit()
+    # Confirmação da operação de inserção de dados e encerramento da conexão com o banco de dados
+    conn.commit()
 
-# except Exception as e:
-#     print(e)
-# finally:
-#     cursor.close()
-#     conn.close()
-#     print('PostgreSQL connection is closed')
+except Exception as e:
+    print(e)
+finally:
+    cursor.close()
+    conn.close()
+    print('PostgreSQL connection is closed')
 
 
 
