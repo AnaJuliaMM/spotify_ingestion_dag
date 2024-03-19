2f5971744988c0c1491e927336e7392b1c738a4f
camilayatabe <camila.yatabe@gmail.com>
2024-03-19 17:30:51 -0300

    fix: remove try/catch inside for

--- dags/ingestao_api_spotify_postgres.py
+++ dags/ingestao_api_spotify_postgres.py
@@ -35,58 +35,53 @@ def requisitar_token():
 # Etapa 2 - Faz a requisição das músicas
 def ingestao(task_instance):
     
-    try:
-            playlist_id = Variable.get("playlist_id")
-            url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
+        try:
+                playlist_id = Variable.get("playlist_id")
+                url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
 
-            token = task_instance.xcom_pull(task_ids="requisitar_token")
-            headers = {
-                "Authorization": f"Bearer {token}"
-            }
+                token = task_instance.xcom_pull(task_ids="requisitar_token")
+                headers = {
+                    "Authorization": f"Bearer {token}"
+                }
 
-            # Enviar solicitação GET para a API do Spotify com o token de acesso no cabeçalho de autorização
-            response = requests.get(url, headers=headers)
-            dados = response.json()
+                # Enviar solicitação GET para a API do Spotify com o token de acesso no cabeçalho de autorização
+                response = requests.get(url, headers=headers)
+                dados = response.json()
 
-            # Verificar se a solicitação foi bem-sucedida (código de status 200)
-            if response.status_code == 200:
+                # Verificar se a solicitação foi bem-sucedida (código de status 200)
+                if response.status_code == 200:
 
-                try:
-                    pg_hook= PostgresHook(postgres_conn_id= 'local_postgres')
-                    # Construção e execução da inserção de dados
-                    for item in dados["items"]:
-                        try:
-                            
-                            # Acessar a chave 'track' do objeto item. Chave que armazena as informações das músicas 
-                            track = item['track']
+                        pg_hook= PostgresHook(postgres_conn_id= 'local_postgres')
+                        # Construção e execução da inserção de dados
+                        for item in dados["items"]:
+                            try:
+                                
+                                # Acessar a chave 'track' do objeto item. Chave que armazena as informações das músicas 
+                                track = item['track']
 
-                            # Extrair os nomes dos artistas e concatenar usando a palavra 'e'
-                            nomes_artistas = [artista['name'] for artista in track['album']['artists']]
-                            artistas_str = ' e '.join(nomes_artistas)
-                            
-                            # Tratar strings que contenham apóstrofos
-                            track_name = track['name'].replace("'", "''")
-                            album_name = track['album']['name'].replace("'", "''")
-                            
-                            pg_hook.run("INSERT INTO Musica (nome, duracao_ms, artistas, nome_album, data_lancamento, total_musicas_album) "
-                                f"VALUES ('{track_name}', {track['duration_ms']}, '{artistas_str}', '{album_name}', TO_DATE('{track['album']['release_date']}', 'YYYY-MM-DD'), {track['album']['total_tracks']})", autocommit= True)
-                            
-                        except Exception as e:
-                            print("Erro ao inserir dados no PostgreSQL:{e}")
-                            raise e
-                         
-                except Exception as e:
-                    logging.exception(e)  
-                    raise e
+                                # Extrair os nomes dos artistas e concatenar usando a palavra 'e'
+                                nomes_artistas = [artista['name'] for artista in track['album']['artists']]
+                                artistas_str = ' e '.join(nomes_artistas)
+                                
+                                # Tratar strings que contenham apóstrofos
+                                track_name = track['name'].replace("'", "''")
+                                album_name = track['album']['name'].replace("'", "''")
+                                
+                                pg_hook.run("INSERT INTO Musica (nome, duracao_ms, artistas, nome_album, data_lancamento, total_musicas_album) "
+                                    f"VALUES ('{track_name}', {track['duration_ms']}, '{artistas_str}', '{album_name}', TO_DATE('{track['album']['release_date']}', 'YYYY-MM-DD'), {track['album']['total_tracks']})", autocommit= True)
+                                
+                            except Exception as e:
+                                logging("Erro ao inserir dados no PostgreSQL:{e}")
+                                raise e
 
-            else:
-                # Se a solicitação não for bem-sucedida, imprima o código de status
-                return response.status_code
-            
-
-    except requests.RequestException as e:
-        # Se ocorrer um erro ao fazer a solicitação, imprima o erro
-        return f"Erro ao fazer solicitação para a API do Spotify:, {e}"
+                else:
+                    # Se a solicitação não for bem-sucedida, imprima o código de status
+                    return response.status_code
+                
+        except requests.RequestException as e:
+            # Se ocorrer um erro ao fazer a solicitação, imprima o erro
+            return f"Erro ao fazer solicitação para a API do Spotify:, {e}"
+        
 
 with DAG ("ingestao_api_spotify_postgres",
         start_date= datetime(2024,2,22),
